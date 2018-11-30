49fb891101dc5cdc0f842098d51b34ba5c68dd4c
Mitzi Morris <mitzi@panix.com>
2018-11-30 15:51:24 -0500

    download pdfs from GitHub stan release page

--- _build.sh
+++ _build.sh
@@ -8,8 +8,6 @@ mkdir -p docs/${VERSION_STRING}
 
 pushd src/functions-reference
 Rscript -e "bookdown::render_book('index.Rmd', output_format='bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', output_format='bookdown::pdf_book')"
-mv _book/_main.pdf ../../docs/stan-functions-reference-${VERSION_STRING}.pdf
 rm _book/*.md
 mv _book ../../docs/${VERSION_STRING}/functions-reference
 rm -rf _book _main.rds stan-manual.css
@@ -17,8 +15,6 @@ popd
 
 pushd src/reference-manual
 Rscript -e "bookdown::render_book('index.Rmd', output_format='bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', output_format='bookdown::pdf_book')"
-mv _book/_main.pdf ../../docs/stan-reference-manual-${VERSION_STRING}.pdf
 rm _book/*.md
 mv _book ../../docs/${VERSION_STRING}/reference-manual
 rm -rf _book _main.rds stan-manual.css
@@ -26,8 +22,6 @@ popd
 
 pushd src/bayes-stats-stan
 Rscript -e "bookdown::render_book('index.Rmd', output_format='bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', output_format='bookdown::pdf_book')"
-mv _book/_main.pdf ../../docs/bayes-stats-stan-${VERSION_STRING}.pdf
 rm _book/*.md
 mv _book ../../docs/${VERSION_STRING}/bayes-stats-stan
 rm -rf _book _main.rds stan-manual.css

7b7f01126f4b4b2dec6f85eb2ed73e0de7e50706
Mitzi Morris <mitzi@panix.com>
2018-11-30 14:53:57 -0500

    added top-level index page

--- _build.sh
+++ _build.sh
@@ -2,7 +2,7 @@
 
 STAN_MAJOR=$1
 STAN_MINOR=$2
-VERSION_STRING=${STAN_MAJOR}.${STAN_MINOR}
+VERSION_STRING=${STAN_MAJOR}_${STAN_MINOR}
 
 mkdir -p docs/${VERSION_STRING}
 
@@ -14,7 +14,6 @@ rm _book/*.md
 mv _book ../../docs/${VERSION_STRING}/functions-reference
 rm -rf _book _main.rds stan-manual.css
 popd
-
 
 pushd src/reference-manual
 Rscript -e "bookdown::render_book('index.Rmd', output_format='bookdown::gitbook')"
@@ -33,3 +32,10 @@ rm _book/*.md
 mv _book ../../docs/${VERSION_STRING}/bayes-stats-stan
 rm -rf _book _main.rds stan-manual.css
 popd
+
+pushd src
+Rscript -e "bookdown::render_book('index.Rmd', output_format='bookdown::html_book')"
+mv _book/index.html ../docs/
+rm -rf _book _main.rds
+popd
+

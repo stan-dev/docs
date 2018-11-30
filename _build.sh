3d308e0774e864497c0ebe58975103beae3c1db0
Mitzi Morris <mitzi@panix.com>
2018-11-29 21:22:53 -0500

    added stan book as bayes-stats-stan

--- _build.sh
+++ _build.sh
@@ -24,3 +24,12 @@ rm _book/*.md
 mv _book ../../docs/${VERSION_STRING}/reference-manual
 rm -rf _book _main.rds stan-manual.css
 popd
+
+pushd src/bayes-stats-stan
+Rscript -e "bookdown::render_book('index.Rmd', output_format='bookdown::gitbook')"
+Rscript -e "bookdown::render_book('index.Rmd', output_format='bookdown::pdf_book')"
+mv _book/_main.pdf ../../docs/bayes-stats-stan-${VERSION_STRING}.pdf
+rm _book/*.md
+mv _book ../../docs/${VERSION_STRING}/bayes-stats-stan
+rm -rf _book _main.rds stan-manual.css
+popd

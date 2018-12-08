174b6ab4edf7be9ddfeb9cfa3650d1f734a18235
Mitzi Morris <mitzi@panix.com>
2018-12-08 16:39:29 -0500

    fixing build script

--- build.py
+++ build.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
-
 """
-build and tag specified document(s)
-in specified format(s)
-with specified MAJOR, MINOR version
+build docs for specified MAJOR, MINOR version
+and specified document (optional), format (optional)
+when document and format not specified, builds all documents in all formats.
+
+arg 1: MAJOR version number
+arg 2: MINOR version number
+optional arg 3: specified document or "all"
+optional arg 4: specified format or "all"
 """
 
+import glob
 import os
 import os.path
 import shutil
 import subprocess
 import sys
 import contextlib
+
+all_docs = ("functions-reference", "reference-manual", "bayes-stats-stan")
+all_formats = ("html", "pdf")
 
 @contextlib.contextmanager
 def pushd(new_dir):
@@ -21,35 +29,51 @@ def pushd(new_dir):
     os.chdir(previous_dir)
 
 def shexec(command):
-    print(command)
     returncode = subprocess.call(command, shell=True)
     if returncode != 0:
         raise FailedCommand(returncode, command)
     return returncode
 
-def make_docs(docspath, version, document, format="all"):
+def safe_rm(fname):
+    if os.path.exists(fname):
+        os.remove(fname)
+
+def safe_rmdir(fname):
+    [safe_rm(f) for f in glob.glob(os.path.join(fname,"*"))]
+    [safe_rm(f) for f in glob.glob(os.path.join(fname,".*"))]
+    if os.path.exists(fname):
+        os.rmdir(fname)
+
+def make_docs(docspath, version, document, formats):
     path = os.getcwd()
     srcpath = os.path.join(path, "src", document)
     with pushd(srcpath):
-        if (format == "all" or format == "html"):
-            command = "Rscript -e \"bookdown::render_book(\'index.Rmd\', output_format=\'bookdown::gitbook\')\""
-            shexec(command)
-        if (format == "all" or format == "pdf"):
+        if ("pdf" in formats):
             command = "Rscript -e \"bookdown::render_book(\'index.Rmd\', output_format=\'bookdown::pdf_book\')\""
             shexec(command)
             srcpdf = os.path.join(srcpath, "_book", "_main.pdf")
             pdfname = ''.join([document,'-',version,".pdf"])
             pdfpath = os.path.join(docspath, pdfname)
             shutil.move(srcpdf, pdfpath)
-        shexec("rm _book/*.md _main.rds")  # cleanup after bookdown
-        if (format == "all" or format == "html"):
+        if ("html" in formats):
+            command = "Rscript -e \"bookdown::render_book(\'index.Rmd\', output_format=\'bookdown::gitbook\')\""
+            shexec(command)
+            [safe_rm(f) for f in ("stan-manual.css", "_main.rds")]
+            [safe_rm(f) for f in glob.glob(os.path.join("_book","*.md"))]
             htmlpath = os.path.join(docspath, document)
             command = ' '.join(["mv _book", htmlpath])
             shexec(command)
         else:
-            shexec("rm -r _book")
+            [safe_rm(f) for f in ("stan-manual.css", "_main.rds")]
+            shutil.rmtree("_book", ignore_errors=True)
+
+def make_index_page(docset, formats):
+    # TODO: generate index.md based on current and new docs
+    # command = "Rscript -e \"bookdown::render_book(\'index.md\', output_format=\'bookdown::html_page\')\""
 
 def main():
+    global all_docs
+    global all_formats
     if (len(sys.argv) > 2):
         stan_major = long(sys.argv[1])
         stan_minor = long(sys.argv[2])
@@ -60,7 +84,6 @@ def main():
     stan_version = '_'.join([str(stan_major), str(stan_minor)])
     path = os.getcwd()
     docspath = os.path.join(path, "docs", stan_version)
-    print "The versioned docs dir is %s" % docspath
     if (not(os.path.exists(docspath))):
         try:  
             os.makedirs(docspath);
@@ -69,7 +92,7 @@ def main():
         else:  
             print "Successfully created the directory %s " % docspath
 
-    docset = ("functions-reference", "reference-manual", "bayes-stats-stan")
+    docset = all_docs
     if (len(sys.argv) > 3):
         if (sys.argv[3] != "all"):
             if (sys.argv[3] not in docset):
@@ -77,7 +100,7 @@ def main():
                 sys.exit(1)
             docset = (sys.argv[3],)
 
-    formats = ("html", "pdf")
+    formats = all_formats
     if (len(sys.argv) > 4):
         if (sys.argv[4] != "all"):
             if (sys.argv[4] not in formats):
@@ -88,11 +111,10 @@ def main():
     if (len(sys.argv) > 5):
         print "Unused arguments:  %s" % ' '.join(sys.argv[5: ])
 
+    make_index_page(docset, formats)
     for doc in docset:
-        print doc
-        for form in formats:
-            print form
-            make_docs(docspath, stan_version, doc, form)
-        
+        make_docs(docspath, stan_version, doc, formats)
+
+
 if __name__ == "__main__":
     main()

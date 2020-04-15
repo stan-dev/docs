fee2a24ae4f3df2c69b09a8f203612a705524fc2
breckbaldwin <git@lingpipe.com>
2020-04-15 19:25:51 -0400

    latest proposed footnote structure

--- src/stan-users-guide/simple_linear_regression/run.R
+++ src/stan-users-guide/simple_linear_regression/run.R
@@ -1,22 +1,32 @@
 library("cmdstanr")
 
-#Run from command line: Rscript regression_1.R
-#If running from RStudio remember to set the working directory 
+#set_cmdstan_path(paste(Sys.getenv("HOME"),"/.cmdstanr/cmdstan",sep=''))
+
+# Run from command line: Rscript regression_1.R
+# If running from RStudio remember to set the working directory 
 # >Session>Set Working Directory>To Source File Location
 
 #generate data
 alpha <- 2
 beta <- 3
-sigma <- 5
+sigma <- 1
 N <- 100
 x <- runif(N, 0, 10)
-y <- rnorm(N, alpha + beta*x, sigma)
-stan_data <- list(N=N, x=x, y=y)
+y <- rnorm(N, alpha + beta * x, sigma)
 
-model = cmdstan_model('regression_1.stan')
+stan_data <- list(N = N, x = x, y = y)
 
-fit <- model$sample(data = stan_data,num_chains=4)
+print(sprintf("generating parameters are: alpha=%.1f, beta=%.1f, sigma=%.1f, N=%d",
+              alpha,beta,sigma,N))
 
+model <- cmdstan_model('regression_naive.stan')
+fit <- model$sample(data = stan_data, num_chains = 4, output_dir = "output")
+print(paste("running stan executable: ",model$exe_file()))
 print(fit$summary())
 
+#=============runs efficient version of regression_naive.stan========
 
+model2 <- cmdstan_model('regression_efficient.stan')
+fit2 <- model2$sample(data = stan_data, num_chains = 4, output_dir = "output")
+print(paste("running stan executable: ",model2$exe_file()))
+print(fit2$summary())
--- src/stan-users-guide/simple_linear_regression/run.py
+++ src/stan-users-guide/simple_linear_regression/run.py
@@ -5,16 +5,31 @@ import numpy
 
 # Run from command line: Python regression_1.py
 
-alpha = 2
-beta = 3
-sigma = 5
+alpha = 2.0
+beta = 3.0
+sigma = 5.0
 n = 100
 x = numpy.random.uniform(size=n)
 y = numpy.random.normal(size=n, loc=alpha + beta * x, scale=sigma)
 
+output = "generating parameters are: alpha={:.1f}, beta={:.1f}, sigma={:.1f}, n={:d}" 
+
+print(output.format(alpha,beta,sigma,n))
+
 stan_data = {'N': n, 'x': x, 'y': y}
 
-stan_program = CmdStanModel(stan_file='regression_1.stan')
+stan_program = CmdStanModel(stan_file='regression_naive.stan')
 stan_program.compile()
-fit = stan_program.sample(data=stan_data, output_dir='.')
+fit = stan_program.sample(data=stan_data, output_dir='output')
+print("running stan executable: ",stan_program.exe_file)
 print(fit.summary())
+
+#=============runs efficient version========
+
+stan_program2 = CmdStanModel(stan_file='regression_efficient.stan')
+stan_program2.compile()
+fit2 = stan_program2.sample(data=stan_data, output_dir='output')
+print("running stan executable: ",stan_program2.exe_file)
+print(fit2.summary())
+
+

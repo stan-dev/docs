0875f17b88617460b45e2c1c82dc9944e2c71864
Nicusor Serban <48496524+serban-nicusor-toptal@users.noreply.github.com>
2020-04-22 22:51:29 +0200

    Update add_links.sh
    Ensure add_links.sh uses python3

--- add_links.sh
+++ add_links.sh
@@ -10,9 +10,9 @@ mkdir docs/"$directory"_tmp
 
 (cd docs/"$directory"; tar cf - .) | (cd docs/"$directory"_tmp; tar xvf - )
 
-python link_to_latest.py docs/"$directory"_tmp/stan-users-guide "Stan User's Guide"
-python link_to_latest.py docs/"$directory"_tmp/reference-manual "Stan Reference Manual"
-python link_to_latest.py docs/"$directory"_tmp/functions-reference "Stan Functions Reference"
+python3 link_to_latest.py docs/"$directory"_tmp/stan-users-guide "Stan User's Guide"
+python3 link_to_latest.py docs/"$directory"_tmp/reference-manual "Stan Reference Manual"
+python3 link_to_latest.py docs/"$directory"_tmp/functions-reference "Stan Functions Reference"
 
 mv docs/"$directory" docs/"$directory"_bak
 mv docs/"$directory"_tmp/ docs/"$directory"

c63fc318241d89b2e7d025cfcf375c593047f283
breckbaldwin <git@lingpipe.com>
2020-04-29 21:01:25 -0400

    syntax fixes

--- src/stan-users-guide/simple_linear_regression/run.R
+++ src/stan-users-guide/simple_linear_regression/run.R
@@ -1,72 +1,65 @@
 library("cmdstanr")
 
-#set_cmdstan_path(paste(Sys.getenv("HOME"),"/.cmdstanr/cmdstan",sep=''))
-
-# Run from command line: Rscript regression_1.R
-# If running from RStudio remember to set the working directory 
+# set_cmdstan_path(paste(Sys.getenv("HOME"),"/.cmdstanr/cmdstan",sep=''))
+# Run from command line: Rscript run.R
+# If running from RStudio remember to set the working directory
 # >Session>Set Working Directory>To Source File Location
 
 #simulate data
 alpha_s <- 2
 beta_s <- 3
 sigma_s <- 1
-N <- 1000
-x <- runif(N, 0, 10)
-y <- rnorm(N, alpha_s + beta_s * x, sigma_s)
+n <- 1000
+x <- runif(n, 0, 10)
+y <- rnorm(n, alpha_s + beta_s * x, sigma_s)
 
-stan_data <- list(N = N, x = x, y = y)
-
-print(sprintf("simulation parameters are: alpha_s=%.1f, beta_s=%.1f, sigma_s=%.1f, N=%d",
-              alpha_s,beta_s,sigma_s,N))
+print(sprintf(paste("simulation parameters are: alpha_s=%.1f",
+              "beta_s=%.1f, sigma_s=%.1f, n=%d"),
+              alpha_s, beta_s, sigma_s, n))
 
 #=============runs simplest version==========
-model <- cmdstan_model('regression_naive.stan')
-print(paste("running stan executable: ",model$exe_file()))
-system.time(fit <- model$sample(data = stan_data, num_chains = 4, output_dir = "output"))
-print(fit$summary())
 
-# Note execution time:
-# All 4 chains finished succesfully.
-# Mean chain execution time: 2.0 seconds.
-# Total execution time: 8.2 seconds.
-# user  system elapsed 
-# 8.019   0.435   8.613 
+stan_data <- list(N = n, x = x, y = y)
+model <- cmdstan_model("regression_naive.stan")
+fit <- model$sample(data = stan_data, num_chains = 4, output_dir = "output")
+print(paste("ran stan executable: ", model$exe_file()))
+print(fit$summary())
 
 #=============runs vectorized version, note speed increase==========
 
-model2 <- cmdstan_model('regression_vectorized.stan')
-fit <- model2$sample(data = stan_data, num_chains = 4, output_dir = "output")
+model_2 <- cmdstan_model("regression_vectorized.stan")
+fit_2 <- model_2$sample(data = stan_data, num_chains = 4, output_dir = "output")
+print(paste("ran stan executable: ", model_2$exe_file()))
+print(fit_2$summary())
 
-# note significant speedup
-# All 4 chains finished succesfully.
-# Mean chain execution time: 1.3 seconds.
-# Total execution time: 5.1 seconds.
+#=============runs matrix version with separate intercept==========
 
-#=============runs matrix version with seperate intercept==========
-K <- 1 # 1 column for coefficient on x
-x_matrix <- matrix(ncol = 1, nrow = N)
-x_matrix[,K] = x
-model2 <- cmdstan_model('regression_matrix.stan')
-stan_data_2 = list(N = N, K = K, x = x_matrix, y = y)
-
-fit2 <- model2$sample(data = stan_data_2, num_chains = 4, output_dir = "output")
-print(paste("running stan executable: ", model2$exe_file()))
-print(fit2$summary())
+k <- 1 # 1 column for coefficient on x
+x_matrix <- matrix(ncol = 1, nrow = n)
+x_matrix[, k] <- x
+stan_data_matrix <- list(N = n, K = k, x = x_matrix, y = y)
+model_3 <- cmdstan_model("regression_matrix.stan")
+fit_3 <- model_3$sample(data = stan_data_matrix, num_chains = 4,
+                      output_dir = "output")
+print(paste("ran stan executable: ", model_3$exe_file()))
+print(fit_3$summary())
 
 #=============runs matrix version with integrated intercept==========
-K2 <- 2 
-x_matrix <- matrix(ncol = K2, nrow = N)
-x_matrix[,1] = rep(1,N) # intercept is always 1
-x_matrix[,2] = x # predictor values
-model3 <- cmdstan_model('regression_matrix_intercept_incl.stan')
-stan_data_3 = list(N = N, K = K2, x = x_matrix, y = y)
-fit3 <- model2$sample(data = stan_data_3, num_chains = 4, output_dir = "output")
-print(paste("running stan executable: ", model3$exe_file()))
-print(fit3$summary())
+k_2 <- 2
+x_matrix <- matrix(ncol = k_2, nrow = n)
+x_matrix[, 1] <- rep(1, n) # intercept is always 1
+x_matrix[, 2] <- x # predictor values
+model_4 <- cmdstan_model("regression_matrix_w_intercept.stan")
+stan_data_matrix_w_intercept <- list(N = n, K = k_2, x = x_matrix, y = y)
+fit_4 <- model_4$sample(data = stan_data_matrix_w_intercept,
+                      num_chains = 4, output_dir = "output")
+print(paste("ran stan executable: ", model_4$exe_file()))
+print(fit_4$summary())
 
+#=============runs QR version==========
 
-model4 <- cmdstan_model('regression_QR.stan')
-stan_data_4 = stan_data_3
-fit4 <- model2$sample(data = stan_data_4, num_chains = 4, output_dir = "output")
-print(paste("running stan executable: ", model3$exe_file()))
-print(fit4$summary())
+model_5 <- cmdstan_model("regression_QR.stan")
+fit_5 <- model_5$sample(data = stan_data_matrix_w_intercept,
+                      num_chains = 4, output_dir = "output")
+print(paste("ran stan executable: ", model_5$exe_file()))
+print(fit_5$summary())
--- src/stan-users-guide/simple_linear_regression/run.py
+++ src/stan-users-guide/simple_linear_regression/run.py
@@ -12,16 +12,16 @@ n = 100
 x = numpy.random.uniform(size=n)
 y = numpy.random.normal(size=n, loc=alpha + beta * x, scale=sigma)
 
-output = "generating parameters are: alpha={:.1f}, beta={:.1f}, sigma={:.1f}, n={:d}" 
+output = "generating parameters are: alpha={:.1f}, beta={:.1f}, sigma={:.1f},n={:d}"
 
-print(output.format(alpha,beta,sigma,n))
+print(output.format(alpha, beta, sigma, n))
 
 stan_data = {'N': n, 'x': x, 'y': y}
 
 stan_program = CmdStanModel(stan_file='regression_naive.stan')
 stan_program.compile()
 fit = stan_program.sample(data=stan_data, output_dir='output')
-print("running stan executable: ",stan_program.exe_file)
+print("running stan executable: ", stan_program.exe_file)
 print(fit.summary())
 
 #=============runs efficient version========
@@ -29,7 +29,5 @@ print(fit.summary())
 stan_program2 = CmdStanModel(stan_file='regression_efficient.stan')
 stan_program2.compile()
 fit2 = stan_program2.sample(data=stan_data, output_dir='output')
-print("running stan executable: ",stan_program2.exe_file)
+print("running stan executable: ", stan_program2.exe_file)
 print(fit2.summary())
-
-

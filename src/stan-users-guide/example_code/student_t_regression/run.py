e4c1b4e4c496048e8192dbf2934033e338ce8b7f
breckbaldwin <git@lingpipe.com>
2020-07-28 20:20:16 -0400

    cleaned up, not fitting well

--- src/stan-users-guide/example_code/student_t_regression/run.py
+++ src/stan-users-guide/example_code/student_t_regression/run.py
@@ -1,29 +1,30 @@
 """ Example program that generates data, compiles and runs regression.stan"""
 from cmdstanpy import CmdStanModel
-from numpy.random import uniform
-from scipy.stats import t
+import numpy
+import scipy.stats
 
-# Run from command line: Python regression_1.py
+# Run from command line: Python run.py
 
-alpha_g = 2.0
-beta_g = 3.0
-sigma_g = 1.0
+alpha_true = numpy.random.normal(size=1, loc=0, scale=1)[0]
+beta_true = numpy.random.normal(size=1, loc=0, scale=1)[0]
+sigma_true = abs(numpy.random.normal(size=1, loc=0, scale=1)[0])
 n = 10
-eta_g = n - 2 # degrees of freedom
-x = uniform(size=n)
-y = t.rvs(eta_g, loc=alpha_g + beta_g * x, scale=sigma_g, size=n)
-output = ("generating parameters are: eta_g={:.1f}, alpha_g={:.1f}," +
-          "beta_g={:.1f},sigma_g={:.1f}, n={:d}")
 
-print(output.format(eta_g, alpha_g, beta_g, sigma_g, n))
+nu = n - 2 # degrees of freedom
 
-stan_data = {'N': n, 'x': x, 'y': y}
+x = numpy.random.uniform(size=n)
+y = scipy.stats.t.rvs(nu, loc=alpha_true + beta_true * x, scale=sigma_true, size=n)
+stan_data = {'N': n, 'x': x, 'y': y, 'nu': nu}
 
-stan_program = CmdStanModel(stan_file='regression_student_t.stan')
+stan_program = CmdStanModel(stan_file='student_t_regression.stan')
 stan_program.compile()
 fit = stan_program.sample(data=stan_data, output_dir='output')
 print("running stan executable: ", stan_program.exe_file)
 print(fit.summary())
+
+output = ("generating parameters are: \nnu={:.1f} \nalpha_true={:.1f}" +
+          "\nbeta_true={:.1f} \nsigma_true={:.1f} \nn={:d}\n")
+print(output.format(nu, alpha_true, beta_true, sigma_true, n))
 
 
 

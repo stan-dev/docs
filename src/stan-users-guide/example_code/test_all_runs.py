b768cea6b28026af09575b1c4a11eee90d7e6004
breckbaldwin <git@lingpipe.com>
2020-08-01 12:33:46 -0400

    rename

--- src/stan-users-guide/example_code/test_all_runs.py
+++ src/stan-users-guide/example_code/test_all_runs.py
@@ -1,9 +1,10 @@
+"""End to end testing program for run.py and run.R programs in sibling directories.  See README.txt for details."""
 import os
 import subprocess
 import sys
 
 def run_script(dir_name, script_call, file_name):
-    print("processing " + dir_name)
+    print("Testing: " + dir_name + "/" + file_name)
     prev_dir = os.getcwd()
     os.chdir(dir_name)
     result = subprocess.run([script_call, file_name], check=True)
@@ -13,13 +14,13 @@ def run_script(dir_name, script_call, file_name):
 
 parent_dir = os.getcwd()
 try:
-    if (len(sys.argv) == 1):
+    if (len(sys.argv) == 1): #no file name given
         print("testing all examples")
         for dir_name in os.listdir(os.getcwd()):
             if os.path.isfile(dir_name):
                 continue
             run_script(dir_name,"Rscript","run.R")
-
+            run_script(dir_name,"python","run.py")
     else:
         file_path = sys.argv[1]
         dir_file = os.path.split(file_path)[0]
@@ -28,9 +29,11 @@ try:
             run_script(dir_file,"Rscript","run.R")
         elif file_name.endswith(".py"):
             run_script(dir_file,"python","run.py")
-        raise NotImplementedError("Unknown script type: " + file_path)
-except:
-    print("End to end test FAIL")
+        else:
+            raise NotImplementedError("Unknown script type: " + file_path)
+    print("End to end test PASS")
+except Exception as e:
+    print("End to end test FAIL",e.__class__)
 finally:
     os.chdir(parent_dir)
 

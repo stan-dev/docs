8c0a0b20665c8da77693b88c740fbf78a8e1f2d0
breckbaldwin <git@lingpipe.com>
2020-08-01 12:32:32 -0400

    linted

--- src/stan-users-guide/example_code/simple_logistic_regression/run.py
+++ src/stan-users-guide/example_code/simple_logistic_regression/run.py
@@ -4,22 +4,26 @@ from numpy.random import uniform, normal
 from numpy import exp
 from scipy.stats import binom
 
-# Run from command line: Python regression_1.py
-n = 1000
-alpha_truth = normal(size=1, loc=0, scale=1)[0]
-beta_truth = normal(size=1, loc=0, scale=1)[0]
-x = uniform(size=n, low=0, high=10)
-v = alpha_truth + beta_truth * x
-p = 1 / (1 + exp(-v))
-y = binom.rvs(1, p, size=n)
-output = "generating parameters are: alpha_truth={:.1f}, beta_truth={:.1f}, n={:d}"
+# Run from command line: python run.py
+N = 1000
+ALPHA_TRUE = normal(size=1, loc=0, scale=1)[0]
+BETA_TRUE = normal(size=1, loc=0, scale=1)[0]
+X = uniform(size=N, low=0, high=10)
+V = ALPHA_TRUE + BETA_TRUE * X
+P = 1 / (1 + exp(-V))
+Y = binom.rvs(1, P, size=N)
 
-print(output.format(alpha_truth, beta_truth, n))
 
-stan_data = {'N': n, 'x': x, 'y': y}
+stan_data = {'N': N, 'x': X, 'y': Y}
 
 stan_program = CmdStanModel(stan_file='logistic_regression.stan')
 stan_program.compile()
 fit = stan_program.sample(data=stan_data, output_dir='output')
 print("running stan executable: ", stan_program.exe_file)
 print(fit.summary())
+
+output = """generating parameters are:
+ALPHA_TRUE={:.1f}
+BETA_TRUE={:.1f}
+N={:d}"""
+print(output.format(ALPHA_TRUE, BETA_TRUE, N))
--- src/stan-users-guide/example_code/simple_probit_regression/run.R
+++ src/stan-users-guide/example_code/simple_probit_regression/run.R
@@ -1,16 +1,11 @@
 library("cmdstanr")
 
-set.seed(123)
 n <- 1000
 x <- rnorm(n)
 alpha_true <- rnorm(1, mean = 0, sd = 1)
 beta_true <-  rnorm(1, mean = 0, sd = 1)
 p <- pnorm(alpha_true + beta_true * x)
 y <- rbinom(n, 1, p)
-
-print(sprintf(paste("simulation parameters are: alpha_true=%.1f",
-              "beta_true=%.1f, n=%d"),
-              alpha_true, beta_true, n))
 
 stan_data <- list(N = n, x = x, y = y)
 
@@ -21,3 +16,7 @@ fit <- model$sample(data = stan_data,
                     output_dir = "output")
 print(paste("ran stan executable: ", model$exe_file()))
 print(fit$summary())
+
+cat(sprintf(paste("simulation parameters are: \nalpha_true=%.1f",
+              "\nbeta_true=%.1f \nn=%d"),
+              alpha_true, beta_true, n))

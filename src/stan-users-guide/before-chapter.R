da2711aea2a099561d97c212ef0ba0eb21c68d28
Mitzi Morris <mitzi@panix.com>
2023-11-22 19:28:22 -0500

    part 1 renders

--- src/stan-users-guide/before-chapter.R
+++ src/stan-users-guide/before-chapter.R
@@ -1,30 +1,6 @@
-# LIBRARY CONFIGURATION
-
 library(dplyr)
 library(ggplot2)
 library(kableExtra)
 
-library(knitr)
-  knitr::opts_chunk$set(
-    cache = TRUE,
-    collapse = TRUE,
-    comment = NA,
-    dev = "png",
-    dpi = 150,
-    echo = FALSE,
-    fig.align = "center",
-    fig.width = 6,
-    fig.asp = 0.618,  # 1 / phi
-    fig.show = "hold",
-    include = TRUE,
-    message = FALSE,
-    out.width = "70%",
-    tidy = FALSE,
-    warning = FALSE
-  )
-
-# GENERAL R CONFIGURATION
-
 options(digits = 2)
 options(htmltools.dir.version = FALSE)
-

49a0a241c4cd653a6158f27547afd2f6ca9812db
Brian Ward <bward@flatironinstitute.org>
2024-05-15 12:43:31 -0400

    Rewrite latex with help

--- gen_index.py
+++ gen_index.py
@@ -54,7 +54,7 @@ def write_index_page(index):
 
             escaped_name = name.replace("\\", "\\\\").replace("*", "\\*")
             f.write(f"**{escaped_name}**:\n\n")
-            for link, entry in sorted(links, key=lambda x: x[1].lower()):
+            for link, entry in sorted(links, key=lambda x: x[1].lower() + x[0]):
                 f.write(f" - [{entry}]({link})\n")
             f.write("\n\n")
 
